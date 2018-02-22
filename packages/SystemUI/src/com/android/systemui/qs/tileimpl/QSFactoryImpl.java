215a3deca968679700102077f86cc9eb66762669
bigrushdog <randall.rushing@gmail.com>
2018-02-21 16:16:21 -0800

    Introduce Theme tile
    Long press tile to change between Style (dark/light/auto ui)
    mode and Accent (#FruityPebbles) mode.
    Click to show detail view.
    
    Change-Id: Ic9ca2a3305b4b79120d7ffca607477636fbbb5c5
    
     Conflicts:
    	packages/SystemUI/res/values/du_strings.xml
    	packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
    
    Change-Id: Ifa1e1cb6f92afbfa2fd2839b1c7586a06be5c339

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -43,6 +43,7 @@ import com.android.systemui.qs.tiles.LocationTile;
 import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ThemeTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.SmartPixelsTile;
 import com.android.systemui.qs.tiles.UiModeNightTile;
@@ -215,6 +216,8 @@ public class QSFactoryImpl implements QSFactory {
                 return mSmartPixelsTileProvider.get();
             case "gaming":
                 return mGamingModeTileProvider.get();
+            case "theme":
+                return new ThemeTile(mHost);
         }
 
         // Intent tiles.
